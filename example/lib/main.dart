97349427e7543482ca17b50f08e8ea9c7788a72f
Abhay Patel <abhay.patel@otpless.com>
2025-07-15 16:46:57 +0530

    * parking

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -35,7 +35,7 @@ class _MyAppState extends State<MyApp> {
   String otpLength = "";
   String expiry = "";
 
-  String appId = "your_client_id";
+  String appId = "AKA12CSMFGN2YYQHOHNB";
 
   @override
   void initState() {
